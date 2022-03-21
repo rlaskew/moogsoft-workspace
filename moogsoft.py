dc49a12d9d097eec5a54da11936a2560c922a5f3
Richard Askew <richardaskew@yahoo.com>
2022-03-21 17:13:42 +0000

    various updates for moving to use config file

---
+++ moogsoft.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# from https://martin-thoma.com/configuration-files-in-python/
+import preprocessing
+
+myhost = {
+   "hostname": "txplanserver1"
+   ,"set_duration": 150
+   ,"set_severity": 5
+   ,"set_description": "txplanserver1 is down"
+   ,"clear_duration": 30
+   ,"clear_severity": 0
+}
